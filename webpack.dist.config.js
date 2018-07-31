6b67dd260f00349c70704cc29e2f498651c20d1b
H3rN4n <desouza030689@gmail.com>
2018-07-30 22:39:04 -0300

    build fix

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -12,14 +12,15 @@ config.plugins = config.plugins.concat([
 
   // Reduces bundles total size
   new webpack.optimize.UglifyJsPlugin({
-    mangle: {
-
+    mangle:
+    //{
+      false
       // You can specify all variables that should not be mangled.
       // For example if your vendor dependency doesn't use modules
       // and relies on global variables. Most of angular modules relies on
       // angular global variable, so we should keep it unchanged
-      except: ['$super', '$', 'exports', 'require', 'angular']
-    }
+      //except: ['$super', '$', 'exports', 'require', 'angular']
+    //}
   })
 ]);
 
